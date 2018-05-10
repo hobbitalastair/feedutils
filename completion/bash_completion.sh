68e4ae9a0cdc51fca398a1bd3f3cb13e22b9b094
Alastair Hughes <hobbitalastair@gmail.com>
2018-05-11 09:15:57 +1200

    bash_completion: for feed-read, only list the unread feeds

--- completion/bash_completion.sh
+++ completion/bash_completion.sh
@@ -12,6 +12,20 @@ _feed_list()
     return 0
 }
 
+_feed_list_unread()
+{
+    [ -z "${FEED_DIR}" ] && FEED_DIR="${XDG_CONFIG_DIR:-${HOME}/.config}/feeds/"
+    [ ! -d "${FEED_DIR}" ] && return 1
+
+    local cur prev OPTS
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    local IFS=$'\n'
+    compopt -o filenames
+    COMPREPLY=( $(compgen -W "$(for entry in "${FEED_DIR}"/*/entry/*; do [ ! -f "${entry}/read" ] && printf '%s\n' "${entry}" | rev | cut -d/ -f3 | rev; done)" -- $cur) )
+    return 0
+}
+
 complete -F _feed_list feed-delete
-complete -F _feed_list feed-read
+complete -F _feed_list_unread feed-read
 complete -F _feed_list feed-update
